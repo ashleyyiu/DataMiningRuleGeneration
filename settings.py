605e5f77721dff1bf7930fd464dae38d4679b754
ashleyyiu <ashley.yiu14@gmail.com>
2016-09-16 19:04:45 -0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,3 @@
+# ASSOCIATION CONSTRUCTION SETTINGS
+MIN_SUPPORT = 0.3                        # FLOAT: The minimum support value
+MIN_CONFIDENCE = 0.75                # FLOAT: The minimum confidence value
